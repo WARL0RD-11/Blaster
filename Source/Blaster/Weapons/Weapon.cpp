10b2ba51a5030826a5b92c1f9b9771cf426f476e
Nishant Verma <vnishant148@gmail.com>
2025-05-29 11:26:31 -0600

    Update IN weapon .cpp to test

--- Source/Blaster/Weapons/Weapon.cpp
+++ Source/Blaster/Weapons/Weapon.cpp
@@ -57,14 +57,14 @@ void AWeapon::BeginPlay()
 
 void AWeapon::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (GEngine)
+	/*if (GEngine)
 	{
 		GEngine->AddOnScreenDebugMessage(
 			-1, 15.0f,
 			FColor::Yellow,
 			FString::Printf(TEXT("Overlap Call"))
 		);
-	}
+	}*/
 	ABlasterCharacter* BlasterCharacter = Cast<ABlasterCharacter>(OtherActor);
 
 	if (BlasterCharacter)
